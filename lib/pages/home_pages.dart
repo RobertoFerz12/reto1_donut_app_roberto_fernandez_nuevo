4e3c26f0eaadd9bae860a06373260e5fbf850ecb
RobertoFerz12 <rcarlos16fp@gmail.com>
2024-10-10 14:41:49 -0600

    mas_cambios

---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:reto1_donut_app_roberto_fernandez_nuevo/tab/burguer_tab.dart';
+import 'package:reto1_donut_app_roberto_fernandez_nuevo/tab/donut_tab.dart';
+import 'package:reto1_donut_app_roberto_fernandez_nuevo/tab/pancake_tab.dart';
+import 'package:reto1_donut_app_roberto_fernandez_nuevo/tab/pizza_tab.dart';
+import 'package:reto1_donut_app_roberto_fernandez_nuevo/tab/smoothie_tab.dart';
+import 'package:reto1_donut_app_roberto_fernandez_nuevo/utils/my_tab.dart';
+
+class HomePages extends StatefulWidget {
+  const HomePages({super.key});
+
+  @override
+  State<HomePages> createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  List<Widget> myTabs = [
+    const MyTab(
+      iconPath: 'lib/icons/donut.png',
+    ),
+    //tab
+//burger tab
+    const MyTab(
+      iconPath: 'lib/icons/burger.png',
+    ),
+//smothie tab
+    const MyTab(
+      iconPath: 'lib/icons/smoothie.png',
+    ),
+//pancake tab
+    const MyTab(
+      iconPath: 'lib/icons/pancakes.png',
+    ),
+//pizza tab
+    const MyTab(
+      iconPath: 'lib/icons/pizza.png',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: IconButton(
+              icon: Icon(Icons.menu, color: Colors.grey[800], size: 36),
+              onPressed: () {
+                print("hola mundo");
+              },
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(
+                Icons.person,
+                color: Colors.grey[800],
+                size: 36,
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto I want to eat
+            const Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                children: [
+                  Text(
+                    "I want to ",
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text("Eat",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline)),
+                ],
+              ),
+            ),
+            TabBar(tabs: myTabs),
+            const Expanded(
+                child: TabBarView(
+              children: [
+                DonutTab(),
+                BurguerTab(),
+                SmoothieTab(),
+                PancakeTab(),
+                PizzaTab(),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tab/burguer_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BurguerTab extends StatelessWidget {
+  const BurguerTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: const Center(
+        child: Text('BurguerTab'),
+      ),
+    );
+  }
+}
